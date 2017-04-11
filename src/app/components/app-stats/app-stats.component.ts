fde73e033cf0e9db7b3cd7781d15abed3176f9e0
Jan Kuri <jkuri88@gmail.com>
2017-04-11 18:24:54 +0200

    chore(release): 0.3.17

--- src/app/components/app-stats/app-stats.component.ts
+++ src/app/components/app-stats/app-stats.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SocketService } from '../../services/socket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-stats',
   templateUrl: 'app-stats.component.html'
 })
-export class AppStatsComponent implements OnInit {
-  loadAvg: any;
+export class AppStatsComponent implements OnInit, OnDestroy {
+  messages: Subscription;
+  loadAvg1Min: number;
+  loadAvg5Min: number;
+  loadAvg15Min: number;
 
   constructor(private socket: SocketService) { }
 
   ngOnInit() {
-    this.socket.onMessage().subscribe(data => {
-
+    this.messages = this.socket.onMessage().subscribe(data => {
       if (data.type === 'loadavg') {
-        this.loadAvg = data.message;
+        this.loadAvg1Min = null;
+        this.loadAvg5Min = null;
+        this.loadAvg15Min = null;
+
+        setTimeout(() => {
+          this.loadAvg1Min = data.message[0];
+          this.loadAvg5Min = data.message[1];
+          this.loadAvg15Min = data.message[2];
+        });
       }
 
       this.socket.emit({
@@ -23,5 +34,9 @@ export class AppStatsComponent implements OnInit {
       });
 
     });
+  }
+
+  ngOnDestroy() {
+    this.messages.unsubscribe();
   }
 }
