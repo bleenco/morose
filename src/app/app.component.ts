da2fd277871c8ddb4f2e0ce654e3b031be06001b
Jan Kuri <jkuri88@gmail.com>
2017-04-11 08:14:51 +0200

    refactor(style): colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
 import * as d3 from 'd3';
 
 @Component({
@@ -16,94 +15,42 @@ import * as d3 from 'd3';
   `
 })
 export class AppComponent {
-  debounceTime: number;
-  resizeSub: Subscription;
-  pkgSub: Subscription;
+  origin: string;
   el: HTMLElement;
 
   constructor(private elementRef: ElementRef) {
-    this.debounceTime = 300;
+    this.origin = document.location.origin;
   }
 
   ngOnInit() {
     this.el = this.elementRef.nativeElement.querySelector('.background-svg');
-    this.renderBackground(true);
-
-    this.pkgSub = Observable.timer(2000).timeInterval().repeat()
-      .subscribe(() => this.colorizePackage());
+    this.renderBackground();
   }
 
-  renderBackground(initial: boolean = false): void {
+  renderBackground(): void {
     let w = this.el.clientWidth;
     let h = this.el.clientHeight;
 
-    let svg;
-    let g;
+    let svg = d3.select(this.el).append('svg').attr('width', w).attr('height', h);
+    let g = svg.append('g').attr('transform', 'translate(0, 0)');
 
-    if (initial) {
-      svg = d3.select(this.el).append('svg').attr('width', w).attr('height', h);
-      g = svg.append('g').attr('transform', 'translate(0, 0)');
-    } else {
-      svg = d3.select(this.el).select('svg');
-      g = d3.select(this.el).select('g');
-      svg.attr('width', w).attr('height', h);
-    }
-
-    let data = d3.range(15).map(() => Math.random() * (4 - 3) + 3);
+    let data = [5.7, 8, 9, 9, 10];
     let x = d3.scaleLinear().domain([0, data.length - 1]).range([0, w]);
-    let y = d3.scaleLinear().domain([0, 5]).range([h, 0]);
+    let y = d3.scaleLinear().domain([0, 15]).range([h, 0]);
 
-    let areaMain = d3.area()
+    let area = d3.area()
       .x((d: any, i: number) => x(i))
-      .y0(h)
-      .y1((d: any) => y(d) + 250)
-      .curve(d3.curveNatural);
+      .y0(0)
+      .y1((d: any) => y(d))
+      .curve(d3.curveCardinal);
 
-    if (initial) {
-      g.append('path')
-        .attr('d', areaMain(data as any))
-        .attr('fill', '#181B26');
-    } else {
-      d3.select('path')
-        .transition()
-        .duration(500)
-        .attr('d', areaMain(data as any))
-        .attr('fill', '#181B26');
-    }
-  }
-
-  colorizePackage(): void {
-    let el = this.elementRef.nativeElement.querySelector('.package');
-    let top = d3.select(el).select('#Shape1');
-    let left = d3.select(el).select('#Shape2');
-    let right = d3.select(el).select('#Shape3');
-
-    let colors = ['#EFC75E', '#E7BF55', '#DBB551'];
-
-    top.attr('fill', colors[0]);
-    left.attr('fill', colors[1]);
-    right.attr('fill', colors[2]);
-
-    top.transition().delay(500).duration(500).attr('fill', colors[0]);
-    left.transition().delay(500).duration(500).attr('fill', colors[2]);
-    right.transition().delay(500).duration(500).attr('fill', colors[1]);
-
-    top.transition().delay(1000).duration(500).attr('fill', colors[1]);
-    left.transition().delay(1000).duration(500).attr('fill', colors[2]);
-    right.transition().delay(1000).duration(500).attr('fill', colors[0]);
-
-    top.transition().delay(1500).duration(500).attr('fill', colors[1]);
-    left.transition().delay(1500).duration(500).attr('fill', colors[0]);
-    right.transition().delay(1500).duration(500).attr('fill', colors[2]);
+    g.append('path')
+      .attr('d', area(data as any))
+      .attr('fill', '#FFFFFF');
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
-    if (this.resizeSub) {
-      this.resizeSub.unsubscribe();
-    }
 
-    let src = Observable.of(null).delay(this.debounceTime);
-    this.resizeSub = src.subscribe(() => this.renderBackground());
   }
 }
