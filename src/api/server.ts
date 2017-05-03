d70d8afb3dd654a45944748c1cc54c72e5234bee
Jan Kuri <jkuri88@gmail.com>
2017-05-03 13:59:52 +0200

    refactor(stats): remove statistic entirely

--- src/api/server.ts
+++ src/api/server.ts
@@ -5,8 +5,6 @@ import * as logger from './logger';
 import * as fs from './fs';
 import * as utils from './utils';
 import { initializeStorage } from './storage';
-import { ISocketServerOptions, SocketServer } from './socket';
-import { loadAverage, networkUtilization } from './system';
 import { Observable } from 'rxjs';
 
 export function start(): void {
@@ -18,42 +16,6 @@ export function start(): void {
       app.use(cors());
       app.use(router);
       app.listen(config.port, () => logger.info(`server running on port ${config.port}`));
-
-      let socketOptions: ISocketServerOptions = {
-        port: config.wsPort,
-        ssl: config.ssl,
-        sslKey: config.sslKey,
-        sslCert: config.sslCert
-      };
-      let socketServer = new SocketServer(socketOptions);
-
-      socketServer.start();
-      socketServer.connections.subscribe(conn => {
-        conn.next({ type: 'status', message: 'connected' });
-
-        let loadavg = loadAverage().subscribe(loadAvg => {
-          conn.next({ type: 'loadavg', message: loadAvg });
-        });
-
-        let netutil = networkUtilization().subscribe(data => {
-          conn.next({ type: 'netutil', message: data });
-        });
-
-        conn.subscribe(data => {
-          data = JSON.parse(data);
-
-          if (data.type === 'close') {
-            conn.unsubscribe();
-            loadavg.unsubscribe();
-            netutil.unsubscribe();
-          }
-        }, err => {
-          console.error(err);
-        }, () => {
-          loadavg.unsubscribe();
-          netutil.unsubscribe();
-        });
-      });
   });
 }
 
