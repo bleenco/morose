bb51df2871d2728d6a5f08b9df3869c45ebed987
Jan Kuri <jkuri88@gmail.com>
2017-05-17 15:30:57 +0200

    test(e2e): remove uneeded messages

--- tests/e2e/setup/010-build.ts
+++ tests/e2e/setup/010-build.ts
@@ -2,7 +2,5 @@ import { npm } from '../utils/process';
 
 export default function() {
   return Promise.resolve()
-    .then(() => console.log('Building morose...'))
-    .then(() => npm('run', 'build:prod'))
-    .then(() => console.log('Done.'));
+    .then(() => npm('run', 'build:prod'));
 }
--- tests/e2e/setup/100-npm-link.ts
+++ tests/e2e/setup/100-npm-link.ts
@@ -2,7 +2,5 @@ import { npm } from '../utils/process';
 
 export default function() {
   return Promise.resolve()
-    .then(() => console.log('Linking morose...'))
-    .then(() => npm('link'))
-    .then(() => console.log('Done.'));
+    .then(() => npm('link'));
 }
