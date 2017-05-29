3d93f54c7ea87fd6621f1596e0444c502c1efe1a
Izak Lipnik <izak.lipnik@gmail.com>
2017-05-29 09:36:51 +0200

    test(): fixed test for npm publish

--- tests/e2e/tests/020_packages/010_unauth_publish.ts
+++ tests/e2e/tests/020_packages/010_unauth_publish.ts
@@ -3,15 +3,13 @@ import { createPackageJson } from '../../utils/utils';
 
 export default function() {
   return Promise.resolve()
-    .then(() => {
-      createPackageJson('package.json')
-      .then(() => npmPublish())
-      .then(res => {
-        if (res.code !== 0 && res.stderr.indexOf('code ENEEDAUTH') !== -1) {
-          return Promise.resolve();
-        }
-        return Promise.reject();
-      })
-      .catch(() => Promise.reject());
-  });
+    .then(() => createPackageJson('package.json'))
+    .then(() => npmPublish())
+    .then(res => {
+      if (res.code !== 0 && res.stderr.indexOf('code ENEEDAUTH') !== -1) {
+        return Promise.resolve();
+      }
+      return Promise.reject();
+    })
+    .catch(() => Promise.reject());
 }
