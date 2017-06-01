03419282b36f3ad697a11b5db64139e871edc442
Izak Lipnik <izak.lipnik@gmail.com>
2017-06-01 09:15:25 +0200

    fix(): npm publish new version fix and added test for installing previous version.

--- tests/e2e/tests/020_publish/010_unauth_publish.ts
+++ tests/e2e/tests/020_publish/010_unauth_publish.ts
@@ -3,7 +3,7 @@ import { createPackageJson } from '../../utils/utils';
 
 export default function() {
   return Promise.resolve()
-    .then(() => createPackageJson('package.json', 'test-package'))
+    .then(() => createPackageJson('package.json', 'test-package', '0.0.1'))
     .then(() => npmPublish())
     .then(res => {
       if (res.code !== 0 && res.stderr.indexOf('code ENEEDAUTH') !== -1) {
