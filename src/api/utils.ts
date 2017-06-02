6ae40fdb9dbbf81d325e4bad84f11c3a37c9f10d
Jan Kuri <jkuri88@gmail.com>
2017-06-02 09:20:20 +0200

    fix(route): get random packages

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -75,8 +75,6 @@ export function writeAuth(secret: string): Promise<null> {
 }
 
 export function getRandomInt(min: number, max: number): number {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
