04a85de0e9968772c4d94c93c7776861484d6bf5
Jan Kuri <jkuri88@gmail.com>
2017-06-06 08:53:09 +0200

    feat(version): print correct version in footer

--- src/app/components/app-foot/app-foot.component.ts
+++ src/app/components/app-foot/app-foot.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+const json = require('../../../../package.json');
 
 @Component({
   selector: 'app-foot',
   templateUrl: 'app-foot.component.html'
 })
-export class AppFootComponent { }
+export class AppFootComponent implements OnInit {
+  version: string;
+
+  ngOnInit() {
+    this.version = json.version;
+  }
+}
