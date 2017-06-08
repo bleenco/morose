4b93d276c79317ee755250605787dddf687e090d
Izak Lipnik <izak.lipnik@gmail.com>
2017-06-08 21:20:34 +0200

    test(): added e2e tests for npm unpublish

--- tests/e2e/tests/080_unpublish/030_unpublish_scoped_package.ts
+++ tests/e2e/tests/080_unpublish/030_unpublish_scoped_package.ts
@@ -6,14 +6,14 @@ export default function() {
   let authPath = 'morose/auth.json';
 
   return Promise.resolve()
-    .then(() => createPackageJson('package.json', 'test-package', '0.0.1'))
+    .then(() => createPackageJson('package.json', '@bleenco/test-package', '0.0.1'))
     .then(() => npmLogin('admin', 'blabla', 'foo@bar.com'))
     .then(() => npmPublish())
     .then(() => execSilent(
-      'npm', ['-q', 'unpublish', 'test-package', '--force', '--fetch-retries', '0']))
+      'npm', ['-q', 'unpublish', '@bleenco/test-package', '--force']))
     .then(() => fs.readJsonFile(authPath))
     .then(authObject => {
-      if (!authObject.packages.find(p => p.name === 'test-package')) {
+      if (!authObject.packages.find(p => p.name === '@bleenco/test-package')) {
         return Promise.resolve();
       }
       return Promise.reject('');
--- tests/e2e/tests/080_unpublish/050_unpublish_version.ts
+++ tests/e2e/tests/080_unpublish/050_unpublish_version.ts
@@ -6,14 +6,19 @@ export default function() {
   let authPath = 'morose/auth.json';
 
   return Promise.resolve()
-    .then(() => createPackageJson('package.json', 'test-package', '0.0.1'))
+    .then(() => createPackageJson('package.json', '@bleenco/test-package', '0.0.1'))
     .then(() => npmLogin('admin', 'blabla', 'foo@bar.com'))
     .then(() => npmPublish())
+    .then(() => createPackageJson('package.json', '@bleenco/test-package', '0.0.2'))
+    .then(() => npmPublish())
+    .then(() => createPackageJson('package.json', '@bleenco/test-package', '0.0.3'))
+    .then(() => npmPublish())
     .then(() => execSilent(
-      'npm', ['-q', 'unpublish', 'test-package', '--force', '--fetch-retries', '0']))
+      'npm', ['-q', 'unpublish', '@bleenco/test-package@0.0.2']))
     .then(() => fs.readJsonFile(authPath))
     .then(authObject => {
-      if (!authObject.packages.find(p => p.name === 'test-package')) {
+      let pkg = authObject.packages.find(p => p.name === '@bleenco/test-package');
+      if (pkg && pkg.versions.length === 2) {
         return Promise.resolve();
       }
       return Promise.reject('');
