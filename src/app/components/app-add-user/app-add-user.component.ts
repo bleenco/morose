2adcc9eb44d40a1f1bef80a6cd4d955e3f2c59cd
Izak Lipnik <izak.lipnik@gmail.com>
2017-06-14 12:19:52 +0200

    feat(): adding users

--- src/app/components/app-add-user/app-add-user.component.ts
+++ src/app/components/app-add-user/app-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -6,15 +7,29 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: 'app-add-user.component.html'
 })
 export class AppAddUserComponent implements OnInit {
-  user: any;
+  error: boolean;
+  success: boolean;
+  username: string;
+  password: string;
+  fullname: string;
+  email: string;
 
-  constructor(private auth: AuthService) {
-    this.user = {};
-  }
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit() {
-    this.user = this.auth.getUser();
+  ngOnInit() {}
 
-    console.log(this.user);
+  addUser(e: Event) {
+    e.preventDefault();
+
+    this.auth.addUser(this.username, this.password, this.fullname, this.email).then(res => {
+      if (res) {
+        this.success = true;
+        this.error = false;
+        setTimeout(() => this.router.navigate(['']), 1000);
+      } else {
+        this.error = true;
+        this.success = false;
+      }
+    });
   }
 }
